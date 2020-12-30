6fd96c95964a1bf7d3772f8da8893738b0c7dd82
Um seoyoung <tjdud0123@naver.com>
2020-12-31 02:52:39 +0900

    1 - A - global - 불필요 파일 제거 및 컴포넌트 부분 이미지 에셋추가

--- motiiv-front/src/App.js
+++ motiiv-front/src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Detail from './pages/Detail/Detail';
 import SignUp from './pages/SignUp/SignUp';
 import SignIn from './pages/SignIn/SignIn';
@@ -8,28 +7,58 @@ import Category from './pages/Category/Category';
 import MyMyotiiv from './pages/MyMotiiv/MyMyotiiv';
 import Admin from './pages/Admin/Admin';
 
-import {
-  BrowserRouter as Router, Route, Switch
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-        <Switch>
-          {/* Main & Category & MyMotiiv */}
-          <Route exact path='/main' render={(props)=>(<Main props={props} />)}></Route>
-          <Route exact path='/category' render={(props)=>(<Category props={props} />)}></Route>
-          <Route exact path='/mymotiiv' render={(props)=>(<MyMyotiiv props={props} />)}></Route>
-          {/* SignUp & SignIn */}
-          <Route exact path='/signup' render={(props)=>(<SignUp props={props}/>)}></Route> 
-          <Route exact path='/signin' render={(props)=>(<SignIn props={props}/>)}></Route>
-          {/* Admin */}
-          <Route exact path='/admin' render={(props)=>(<Admin props={props} />)}></Route>
-          {/* Detail */}
-          <Route exact path='/detail/:id' render={(props)=>(<Detail props={props} />)}></Route>
-          {/* Upload */}
-          <Route exact path='/upload' render={(props)=>(<Upload props={props}/>)}></Route>
-          <Route path='/*'>404 NOT FOUND</Route>
-        </Switch>
+    <Switch>
+      {/* Main & Category & MyMotiiv */}
+      <Route
+        exact
+        path="/main"
+        render={props => <Main props={props} />}
+      ></Route>
+      <Route
+        exact
+        path="/category"
+        render={props => <Category props={props} />}
+      ></Route>
+      <Route
+        exact
+        path="/mymotiiv"
+        render={props => <MyMyotiiv props={props} />}
+      ></Route>
+      {/* SignUp & SignIn */}
+      <Route
+        exact
+        path="/signup"
+        render={props => <SignUp props={props} />}
+      ></Route>
+      <Route
+        exact
+        path="/signin"
+        render={props => <SignIn props={props} />}
+      ></Route>
+      {/* Admin */}
+      <Route
+        exact
+        path="/admin"
+        render={props => <Admin props={props} />}
+      ></Route>
+      {/* Detail */}
+      <Route
+        exact
+        path="/detail/:id"
+        render={props => <Detail props={props} />}
+      ></Route>
+      {/* Upload */}
+      <Route
+        exact
+        path="/upload"
+        render={props => <Upload props={props} />}
+      ></Route>
+      <Route path="/*">404 NOT FOUND</Route>
+    </Switch>
   );
 }
 
