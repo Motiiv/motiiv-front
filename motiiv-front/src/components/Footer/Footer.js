472759bc2447c63d36b7ab281bfe19fbeb751eca
Um seoyoung <tjdud0123@naver.com>
2021-01-02 02:23:50 +0900

    7-A-Global- footer 생성

--- motiiv-front/src/components/Footer/Footer.js
+++ motiiv-front/src/components/Footer/Footer.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import logoImg from '../../assets/global/gray_logo.png';
+import { withRouter } from 'react-router-dom';
 
-function Footer() {
-    return (
-        <>
-            
-        </>
-    )
+function Footer({ history }) {
+  const goToPrivacy = () => {
+    history.push('privacy');
+  };
+  const FooterWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 6rem;
+  `;
+
+  const Text = styled.h6`
+    color: ${props => props.theme.gray};
+    font-size: 1.6rem;
+    font-weight: 400;
+    margin: 1.5rem 0;
+  `;
+  const FooterLogo = styled.img`
+    height: 2rem;
+  `;
+  const GoPrivacy = styled.a`
+    color: ${props => props.theme.gray};
+    font-size: 1.6rem;
+    text-decoration: underline;
+    cursor: pointer;
+  `;
+
+  return (
+    <FooterWrapper>
+      <FooterLogo src={logoImg} />
+      <Text>
+        <b>Copyright</b> motiiv All right reserved
+      </Text>
+      <GoPrivacy onClick={goToPrivacy}>개인정보처리방침</GoPrivacy>
+    </FooterWrapper>
+  );
 }
 
-export default Footer
+export default withRouter(Footer);
