e058f9b552dad93784337978f1cc71df6bfe25ef
Say-young <seyoung990525@gmail.com>
2021-01-03 00:07:14 +0900

    12-F-Home-카드 제외 merge

--- motiiv-front/src/App.js
+++ motiiv-front/src/App.js
@@ -8,6 +8,8 @@ import MyMyotiiv from './pages/MyMotiiv/MyMyotiiv';
 import Admin from './pages/Admin/Admin';
 import Navbar from './components/Navbar/Navbar'
 import SwiperBanner from './components/Banner/SwiperBanner';
+import BottomBanner from './components/Banner/BottomBanner';
+import Footer from './components/Footer/Footer'
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
@@ -16,7 +18,6 @@ function App({ props }) {
     <>
       <Navbar/>
       {/* <SwiperBanner props={props} /> */}
-      <SwiperBanner></SwiperBanner>
       <Switch>
         {/* Main & Category & MyMotiiv */}
         <Route
@@ -65,6 +66,8 @@ function App({ props }) {
         ></Route>
         <Route path="/*">404 NOT FOUND</Route>
       </Switch>
+      <BottomBanner/>
+      <Footer/>
     </>
   );
 }
