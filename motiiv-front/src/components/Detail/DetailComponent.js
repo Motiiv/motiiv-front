5fefbc72fc7d548394944d59c8a975e44188db32
jungwook <neity16@daum.net>
2021-01-03 18:11:45 +0900

    프로젝트 구조 변경3

--- motiiv-front/src/components/Detail/DetailComponent.js
+++ motiiv-front/src/components/Detail/DetailComponent.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+
 function DetailComponent() {
-  return <div></div>;
+  return <></>;
 }
 
 export default DetailComponent;
