b720cc27c3583c4e4dfac59ffcb0a3e2657a1ea9
jungwook <neity16@daum.net>
2021-01-05 03:53:09 +0900

    23-A-Category-퍼블리싱 작업중 & Card / ImageSlider 수정

--- motiiv-front/src/components/common/Section/ImageSlider.js
+++ motiiv-front/src/components/common/Section/ImageSlider.js
@@ -1,7 +1,7 @@
-import React,{useRef} from 'react'
+import React, { useRef } from 'react';
 import Card from '../Card/Card';
-import {Swiper, SwiperSlide} from 'swiper/react';
-import styled, {css} from 'styled-components';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import styled, { css } from 'styled-components';
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import 'swiper/swiper.scss';
 import 'swiper/components/navigation/navigation.scss';
@@ -11,57 +11,76 @@ SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 const SliderSection = styled.div`
   display: flex;
-  position : relative;
+  position: relative;
   width: 100%;
+  max-width: 1280px;
+  padding: 0 5.5rem;
   justify-content: center;
   & .swiper-button-prev::after {
     position: absolute !important;
-    left: 0.6rem !important;
-    bottom: 50% !important;
-    font-weight : 2rem !important;
-    width:  3rem !important;
+    right: 0 !important;
+    ${props =>
+      props.size === 'large'
+        ? css`
+            top: -4rem !important;
+          `
+        : css`
+            top: -4.5rem !important;
+          `}
+    font-weight: 2rem !important;
+    width: 3rem !important;
     height: 3rem !important;
     font-size: 1.5rem !important;
-    color: ${({theme}) => theme.darkGray};
+    color: ${({ theme }) => theme.darkGray};
     cursor: pointer !important;
     z-index: 2 !important;
   }
   & .swiper-button-next::after {
     position: absolute !important;
-    left : 0.9rem !important;
-    bottom: 50% !important;
-    font-weight : 2rem !important;
-    width:  3rem !important;
+    left: 2.5rem !important;
+    ${props =>
+      props.size === 'large'
+        ? css`
+            top: -4rem !important;
+          `
+        : css`
+            top: -4.5rem !important;
+          `}
+    font-weight: 2rem !important;
+    width: 3rem !important;
     height: 3rem !important;
     font-size: 1.5rem !important;
-    color: ${({theme}) => theme.darkGray};
+    color: ${({ theme }) => theme.darkGray};
     cursor: pointer !important;
     z-index: 2 !important;
- }
+  }
+  & .swiper-slide {
+    flex: 1 !important;
+  }
 `;
 
-function ImageSlider({SliderObject,type,size}) {
-    const swiperRef = useRef();
-    const num = type=== "top" ? 3 : 4;
-    const space = type === "top" ? 20 : 25;
-    return (
-        <SliderSection>
-        <Swiper
-        spaceBetween = {space}
-        slidesPerView = {num}
+function ImageSlider({ SliderObject, type, size }) {
+  const swiperRef = useRef();
+  const num = type === 'top' ? 3 : 4;
+  const space = type === 'top' ? 20 : 25;
+  return (
+    <SliderSection size={size}>
+      <Swiper
+        spaceBetween={space}
+        slidesPerView={num}
+        loop={true}
         ref={swiperRef}
         navigation
-        style={{position : "static",
-                width: '88.5%',
-                maxWidth: '128rem',}}>
-        {SliderObject.map((obj,idx) => ( 
-            <SwiperSlide >
-                <Card size = {size} key={`card-${idx}`} obj={obj}></Card>
-            </SwiperSlide>
+        style={{ position: 'static', width: '100%' }}
+      >
+        {SliderObject.map((obj, idx) => (
+          <SwiperSlide>
+            <Card size={size} key={`card-${idx}`} obj={obj}></Card>
+          </SwiperSlide>
         ))}
-        </Swiper>
-        </SliderSection>
-    );
+      </Swiper>
+    </SliderSection>
+  );
 }
 
-export default ImageSlider
+export default ImageSlider;
