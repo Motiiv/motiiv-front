8e30c1ec1c2fa728a3f18cf4cf40a2676b383768
jungwook <neity16@daum.net>
2021-01-07 16:32:33 +0900

    마루타

--- motiiv-front/src/components/common/Card/Card.js
+++ motiiv-front/src/components/common/Card/Card.js
@@ -2,66 +2,67 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 import Tag from '../Tag/Tag';
 import { withRouter } from 'react-router-dom';
-import HoverVideoPlayer from 'react-hover-video-player' ; 
+import HoverVideoPlayer from 'react-hover-video-player';
 const CardWrap = styled.div`
-    display : flex;
-    width: 100%;
-    min-width: ${props => (props.size === "large" ? '37.7rem' : '27.4rem')};
-    height : auto;
-    min-height:  ${props=>props.size ==="large" ? "33.5 rem" : "26.7rem" };
-    flex-direction: column;
-    box-shadow: ${props=>props.size ==="large" ? "2px 2px 7px rgba(0, 0, 0, 0.15)" : "none" };
-    border-radius: 1rem;
-    @media ${props => props.theme.mobile}{
-      min-width: 30rem;
-      min-height: 24.2rem;
-    
-
-    }
-    /* height : ${props=>props.size ==="large" ? "32.6rem" : "28rem" };
+  display: flex;
+  width: 100%;
+  min-width: ${props => (props.size === 'large' ? '37.7rem' : '27.4rem')};
+  height: auto;
+  min-height: ${props => (props.size === 'large' ? '33.5 rem' : '26.7rem')};
+  flex-direction: column;
+  box-shadow: ${props =>
+    props.size === 'large' ? '2px 2px 7px rgba(0, 0, 0, 0.15)' : 'none'};
+  border-radius: 1rem;
+  @media ${props => props.theme.mobile} {
+    min-width: 30rem;
+    min-height: 24.2rem;
+  }
+  /* height : ${props => (props.size === 'large' ? '32.6rem' : '28rem')};
     flex-direction: column;    
-    box-shadow: ${props=>props.size ==="large" ? "2px 2px 7px rgba(0, 0, 0, 0.15)" : "none" };
+    box-shadow: ${props =>
+    props.size === 'large' ? '2px 2px 7px rgba(0, 0, 0, 0.15)' : 'none'};
     border-radius: 1rem; */
 `;
 const VideoWrap = styled.div`
-    width: 100%;
-    height: auto;
-    height: ${props => (props.size === 'large' ? '21.2rem' : '15.4rem')};
-    display: flex;
-    align-items:center; 
-    justify-content:center;
-    background : #C4C4C4;
-    border-radius: ${props=>props.size ==="large"? "1rem 1rem 0 0" : "1rem"};
-    position : relative; 
-    @media ${props => props.theme.mobile}{
-      height: 16.8rem;
-    }
-    `;
+  width: 100%;
+  height: auto;
+  height: ${props => (props.size === 'large' ? '21.2rem' : '15.4rem')};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #c4c4c4;
+  border-radius: ${props =>
+    props.size === 'large' ? '1rem 1rem 0 0' : '1rem'};
+  position: relative;
+  @media ${props => props.theme.mobile} {
+    height: 16.8rem;
+  }
+`;
 // const Video = styled.div`
 //     position : absolute;
 //     width: 100%;
 // `;
 
 const TimeContainer = styled.div`
-    position: absolute;
-    right: 1.5rem;
-    bottom: 1.4rem;
-    width: 4.5rem;
-    z-index : 1000;
-    height: 1.8rem;
-    font-size: 1.2rem;
-    padding: 0.3rem 0.5rem;
-    padding-top: 0.4rem;
-    background: rgba(0, 0, 0, 0.6);
-    border-radius: 1rem;
-    display: flex;
-    color: white;
-    justify-content: center;
-    align-items: center;
-    font-family: 'Campton';
-    & + & {
-        margin-left: 1.2rem;
-    }
+  position: absolute;
+  right: 1.5rem;
+  bottom: 1.4rem;
+  width: 4.5rem;
+  z-index: 1000;
+  height: 1.8rem;
+  font-size: 1.2rem;
+  padding: 0.3rem 0.5rem;
+  padding-top: 0.4rem;
+  background: rgba(0, 0, 0, 0.6);
+  border-radius: 1rem;
+  display: flex;
+  color: white;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Campton';
+  & + & {
+    margin-left: 1.2rem;
+  }
 `;
 
 const Views = styled.div`
@@ -75,9 +76,9 @@ const Views = styled.div`
   display: -webkit-box;
   -webkit-line-clamp: 2;
   -webkit-box-orient: vertical;
-  @media ${props => props.theme.mobile}{
-      font-size : 1.2rem;
-    }     
+  @media ${props => props.theme.mobile} {
+    font-size: 1.2rem;
+  }
 `;
 
 const Channel = styled.div`
@@ -94,15 +95,14 @@ const Channel = styled.div`
   display: -webkit-box;
   -webkit-line-clamp: 2;
   -webkit-box-orient: vertical;
-  @media ${props => props.theme.mobile}{
-      font-size : 1.2rem;
-    }
+  @media ${props => props.theme.mobile} {
+    font-size: 1.2rem;
+  }
 `;
 const Video = styled.img`
-    width: 100%;
-    height: 100%;
-    border-radius: 1rem;
-
+  width: 100%;
+  height: 100%;
+  border-radius: 1rem;
 `;
 const TextWrap = styled.div`
   width: 100%;
@@ -111,69 +111,67 @@ const TextWrap = styled.div`
   flex-direction: column;
   background: ${props => (props.size === 'large' ? 'white' : 'transparent')};
   border-radius: 0 0 1rem 1rem;
-  @media ${props => props.theme.mobile}{
-      height: 7.4rem;
-    }
+  @media ${props => props.theme.mobile} {
+    height: 7.4rem;
+  }
 `;
 const Title = styled.div`
-    margin-left : ${props => (props.size === 'large' ? '2rem' : '0')};
-    color : black;
-    font-size: 1.5rem;
-    font-family: 'Spoqa-Han-Sans';
-    overflow: hidden;
-    line-height: 1.7rem;
-    max-height: 4.2rem;
-    height: 3.4rem;
-    margin-top : ${props => (props.size === 'large' ? '1rem' : '0.8rem')};
-    text-overflow: ellipsis;
-    word-break: keep-all;
-    word-wrap: break-word;
-    display: -webkit-box;
-    -webkit-line-clamp: 2;
-    -webkit-box-orient: vertical;
-    @media ${props => props.theme.mobile}{
-      margin-left : 0;
-    }
-    
-    `;
+  margin-left: ${props => (props.size === 'large' ? '2rem' : '0')};
+  color: black;
+  font-size: 1.5rem;
+  font-family: 'Spoqa-Han-Sans';
+  overflow: hidden;
+  line-height: 1.7rem;
+  max-height: 4.2rem;
+  height: 3.4rem;
+  margin-top: ${props => (props.size === 'large' ? '1rem' : '0.8rem')};
+  text-overflow: ellipsis;
+  word-break: keep-all;
+  word-wrap: break-word;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  @media ${props => props.theme.mobile} {
+    margin-left: 0;
+  }
+`;
 const DescriptionContainer = styled.div`
-    display: flex;
-    margin-left: ${props => (props.size === 'large' ? '2rem' : '0')};
-    margin-top : ${props=>props.size ==="large"? "0.9rem" : "0.8 rem"};
-    @media ${props => props.theme.mobile}{
-      margin-left : 0;
-      margin-top : 0.5rem;
-    }
-    `;
+  display: flex;
+  margin-left: ${props => (props.size === 'large' ? '2rem' : '0')};
+  margin-top: ${props => (props.size === 'large' ? '0.9rem' : '0.8 rem')};
+  @media ${props => props.theme.mobile} {
+    margin-left: 0;
+    margin-top: 0.5rem;
+  }
+`;
 const TagContainer = styled.div`
-    display : flex;
-    width: 100%;
-    margin-top :${props=>props.size ==="large"? "1.5rem" : "0.8rem"};
-    margin-left: ${props => (props.size === 'large' ? '2rem' : '1px')};
-    @media ${props => props.theme.mobile}{
-      display : none;
-    }
-    `;
+  display: flex;
+  width: 100%;
+  margin-top: ${props => (props.size === 'large' ? '1.5rem' : '0.8rem')};
+  margin-left: ${props => (props.size === 'large' ? '2rem' : '1px')};
+  @media ${props => props.theme.mobile} {
+    display: none;
+  }
+`;
 // Tag 컴포넌트 만들어서 불러오기
 function Card({ obj, size, history }) {
   return (
     <>
       <CardWrap size={size}>
         <VideoWrap size={size} onClick={() => history.push('/detail/1')}>
-            <HoverVideoPlayer style={{ width: "100%",
-                                        height: "100%",
-                                        borderRadius : "1rem"}}
-                videoSrc={obj.VideoInfo.src}
-                pausedOverlay={
-                    <img width= "100%"
-                    height= "100%"
-                    src="https://user-images.githubusercontent.com/61861809/103748463-82a9d380-5047-11eb-9720-697a1f2de7cd.jpg"/>
-                }
-                loadingOverlay={
-                    <div className="loading-spinner-overlay" />
-                }
-            />
-        {/* <Video src={obj.VideoInfo.src}></Video> */}
+          <HoverVideoPlayer
+            style={{ width: '100%', height: '100%', borderRadius: '1rem' }}
+            videoSrc={obj.VideoInfo.src}
+            pausedOverlay={
+              <img
+                width="100%"
+                height="100%"
+                src="https://user-images.githubusercontent.com/61861809/103748463-82a9d380-5047-11eb-9720-697a1f2de7cd.jpg"
+              />
+            }
+            loadingOverlay={<div className="loading-spinner-overlay" />}
+          />
+          {/* <Video src={obj.VideoInfo.src}></Video> */}
           {/* <iframe
             style={{
               height: '100%',
